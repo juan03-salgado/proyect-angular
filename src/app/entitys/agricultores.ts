f260491d1ef97cdf088da67c6758f977fc60a5b8
juan03-salgado <juanjosesalgadovidal@gmail.com>
2025-10-03 22:44:19 -0600

    proyecto angular

--- src/app/entitys/agricultores.ts
+++
@@ -1,8 +0,0 @@
-export class Agricultores {
-    constructor(public id: number, 
-        public nombre: string, 
-        public email: string, 
-        public telefono: string, 
-        public ubicacion: string) {
-    }
-}
